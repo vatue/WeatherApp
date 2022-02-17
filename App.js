f0b79afec9d4f4f1af7d32853eeb8204333ed85c
vatue <thevatu1@gmail.com>
2022-02-17 12:47:40 +0200

    Example API Call, Unauthorized 401

--- App.js
+++ App.js
@@ -1,5 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+
+axios({
+  method: 'get',
+  url: 'api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=18df0e05d5965b130dbc58e2c9ef47f7',
+}).then((response) => {
+    console.log(response.data);
+  });
+  // testing free weather api
+  // 401 unauthorized
 
 export default function App() {
   return (
